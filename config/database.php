b71a4eba40200063a9c2bec39d162ef49ad8831d
Al Amin <alamin.aiub10@gmail.com>
2025-02-03 11:43:07 +0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = '127.0.0.1';
-$db = 'famesrbd_event';
-$user = 'famesrbd_event';
-$pass = 'NNvHOPeftLC7';
+$db = 'event';
+$user = 'root';
+$pass = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
